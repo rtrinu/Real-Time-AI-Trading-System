1cbb8451e0eef77453c32b1f816e7766103061de
Rohan Binu <rbinu2004@gmail.com>
2026-07-30 17:03:45 +0100

    write 401 log directly to file instead of print

--- src/core/auth.py
+++ src/core/auth.py
@@ -4,6 +4,7 @@ from core.config import settings
 
 def verify_api_key(x_api_key: str = Header(...)):
     if x_api_key != settings.api_key:
-        print(f"FAIL2BAN 401 from invalid API key")
+        with open("/app/logs/access.log", "a") as f:
+            f.write(f"FAIL2BAN 401 from invalid API key\n")
         raise HTTPException(status_code=401, detail="Invalid API key")
     return x_api_key
